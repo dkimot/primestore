91a50f2d416c1cdf3cb3c1c8008f099c46b014e7
Davis Kimoto <davis.kimoto@gmail.com>
2017-03-09 14:27:12 -0700

    added trivial logging

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = (process.env.PROD) ? 80 : 3030;
 
+app.use((req, res, next) => {
+  console.log('Request received');
+});
 app.use(express.static('dist'));
 
 app.listen(PORT, () => {
